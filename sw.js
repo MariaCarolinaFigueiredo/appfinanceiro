949eec63b290af6360fcc7c8942c577fd8be5045
Maria Carolina <mcfnsilva@gmail.com>
2026-09-12 18:06:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nossa-casa-v2';
+const CACHE='nossa-casa-mobile-v5-20260912';
 const CORE=['/','/index.html','/manifest.webmanifest','/logo-nossa-casa.png','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
